cc4037240a1793546a0dcc0b33780f97b901cd7e
Kavia AI <production@kavia.ai>
2026-02-12 12:13:56 +0000

    Checkpoint 5 - cg391109b0

--- frontend_client/src/setupTests.js
+++ frontend_client/src/setupTests.js
@@ -2,4 +2,14 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+
+// jsdom does not implement scrollIntoView; components may call it for UX auto-scroll.
+// Provide a harmless noop so unit tests don't crash.
+// (Using defineProperty avoids "Cannot assign to read only property" in some environments.)
+if (!window.HTMLElement.prototype.scrollIntoView) {
+  Object.defineProperty(window.HTMLElement.prototype, "scrollIntoView", {
+    value: () => {},
+    writable: true,
+  });
+}
